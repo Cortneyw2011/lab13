ccfcc8fb6b92459dad752ec0556219dab58e29b1
Cortney Wilson <cortneyw2011@gmail.com>
2017-03-01 20:16:41 -0500

    Met Lab Requirements

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
   $.get("https://www.reddit.com/r/aww/.json", function(goodness){
     var redditArray = goodness.data.children;
-    
+
     for(var i=0; i<redditArray.length; i++){
       var thumbnail = redditArray[i].data.thumbnail;
       var title = redditArray[i].data.title;
+      var author = redditArray[i].data.author;
+      var score = redditArray[i].data.score;
+      var permalink =  "https://www.reddit.com/" + redditArray[i].data.permalink;
       $('section').append(
-        '<div><h2>' + title +
-        '</h2><img src="'
-        + thumbnail+
-        '"/></div>');
+        '<div><a href =  " '+ permalink +'"  ><h2>' + 'Title: ' + title + '<br>' + 'Author: ' + author+ '<br>' + 'Score: ' + score +
+        '</h2><img src="' + thumbnail+ '"/></a></div>');
+
+
     }
   });
 });
